ba5c3a008cf3c210b2175583412d0ea84b87cc63
algow <algow64@gmail.com>
2023-03-20 08:08:52 +0700

    update dari harto

--- src/main/java/org/hartomoro/Restoran.java
+++ src/main/java/org/hartomoro/Restoran.java
@@ -7,39 +7,43 @@ public class Restoran {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Scanner scanner = new Scanner(System.in); // untuk penjumlahan menu agar maximal 10
-        System.out.println("Selamat Datang di Restoran Bugar Universitas BNAS");
-        System.out.println("----------------------------");
 
-        System.out.print("Masukkan nama pemesan: ");
+
+        System.out.println("Selamat Siang...");
+
+        System.out.print("Pesanan atas nama : ");
         String namaPemesan = input.nextLine();
-        // Input jumlah orang dan nama pemesan
+
         System.out.print("Masukkan jumlah orang: ");
         int jumlahOrang = input.nextInt();
-        System.out.println("--------------------------------------------------------");
+
+        System.out.println("\n");
 
         // Tampilkan menu makanan
-        System.out.println("Menu Makanan:");
+        System.out.println("Menu Spesial hari Ini :");
+        System.out.println("=======================");
         System.out.println("1. Nasi Goreng Spesial (Rp. 9999.99)");
         System.out.println("2. Ayam Bakar Spesial (Rp. 12345.67)");
         System.out.println("3. Steak Sirloin Spesial (Rp. 21108.40)");
         System.out.println("4. Kwetiaw Siram Spesial (Rp. 13579.13)");
         System.out.println("5. Kambing Guling Spesial (Rp. 98765.43)");
+        System.out.println("\n");
 
+        System.out.println("Pesanan anda [batas pesanan 1-10 porsi]");
         // Input jumlah pesanan masing-masing menu
-        System.out.print("Masukkan jumlah pesanan Nasi Goreng Spesial: ");
+        System.out.print("1. Nasi Goreng Spesial: ");
         int nasiGoreng = input.nextInt();
-        System.out.print("Masukkan jumlah pesanan Ayam Bakar Spesial: ");
+        System.out.print("2. Ayam Bakar Spesial: ");
         int ayamBakar = input.nextInt();
-        System.out.print("Masukkan jumlah pesanan Steak Sirloin Spesial: ");
+        System.out.print("3. Steak Sirloin Spesial: ");
         int steakSirloin = input.nextInt();
-        System.out.print("Masukkan jumlah pesanan Kwetiaw Siram Spesial: ");
+        System.out.print("4. Kwetiaw Siram Spesial: ");
         int kwetiawSiram = input.nextInt();
-        System.out.print("Masukkan jumlah pesanan Kambing Guling Spesial: ");
+        System.out.print("5. Kambing Guling Spesial: ");
         int kambingGuling = input.nextInt();
 
         int total = nasiGoreng + ayamBakar + steakSirloin + kwetiawSiram + kambingGuling;
 
-        System.out.print("total pesanan: " + total);
 
         System.out.println("\n");
 
@@ -89,28 +93,26 @@ public class Restoran {
         String formatted7 = df.format(hargaDiskon);
         String formatted8 = df.format(totalPembelianSetelahDiskon);
         String formatted9 = df.format(pembelianPerOrang);
+        System.out.println("\n");
 
-        // Tampilkan hasil
-        System.out.println("----------------------------");
-        System.out.println("Restoran BUGAR");
-        System.out.println("Nama Pemesan : " + namaPemesan);
-        System.out.println("----------------------------");
+
+        System.out.println("Selamat menikmati makanan anda...");
+
         System.out.println("Pembelian :");
-        System.out.println("1. Nasi Goreng Spesial        : " + nasiGoreng + " * Rp. 9999.99 = "+ hargaNasiGoreng);
-        System.out.println("2. Ayam Bakar Spesial         : " + ayamBakar +  " * Rp. 12345.67 = "+ hargaAyamBakar);
-        System.out.println("3. Steak Sirloin Spesial      : " + steakSirloin +  " * Rp. 21108.40 = "+ hargaSteakSirloin);
-        System.out.println("4. Kwetiaw Siram Spesial      : " + kwetiawSiram +  " * Rp. 13579.13 = "+ hargaKwetiawSiram);
-        System.out.println("5. Kambing Guling Spesial     : " + kambingGuling +  " * Rp. 13579.13 = "+ hargaKambingGuling);
-        System.out.println("----------------------------");
-        System.out.printf("Total Pembelian : %.2f", totalPembelian);
+        System.out.println("1. Nasi Goreng Spesial        : " + nasiGoreng + " porsi " + " * Rp. 9999.99 = " + String.format("%.2f", hargaNasiGoreng));
+        System.out.println("2. Ayam Bakar Spesial         : " + ayamBakar +  " porsi " + " * Rp. 12345.67 = "+ String.format("%.2f", hargaAyamBakar));
+        System.out.println("3. Steak Sirloin Spesial      : " + steakSirloin + " porsi " +  " * Rp. 21108.40 = "+ String.format("%.2f", hargaSteakSirloin));
+        System.out.println("4. Kwetiaw Siram Spesial      : " + kwetiawSiram +  " porsi " + " * Rp. 13579.13 = "+ String.format("%.2f", hargaKwetiawSiram));
+        System.out.println("5. Kambing Guling Spesial     : " + kambingGuling +  " porsi " + " * Rp. 13579.13 = "+ String.format("%.2f", hargaKambingGuling) + "   + ");
+        System.out.println("=========================================================================");
+        System.out.println("Total Pembelian " + String.format("%.2f", totalPembelian));
         System.out.println("\n");
-        System.out.printf("Dics 10 persen (Masa Promosi) : %.2f", hargaDiskon);
+        System.out.println("Dics 10 % (Masa Promosi) : " + String.format("%.2f", hargaDiskon) +"                                  -");
+        System.out.println("=========================================================================");
+        System.out.println("Total Pembelian setelah Diskon 10 persen :" + String.format("%.2f", totalPembelianSetelahDiskon));
         System.out.println("\n");
-        System.out.println("----------------------------");
-        System.out.printf("Total Pembelian setelah Diskon 10 persen : RP. : %.2f", totalPembelianSetelahDiskon);
-        System.out.println("\n");
-        System.out.println("----------------------------");
-        System.out.printf("Total Harga Perorang : : RP.  %.2f", pembelianPerOrang);
+        System.out.println("=========================================================================");
+        System.out.println("Total Harga Perorang :" + String.format("%.2f", pembelianPerOrang));
         System.out.println("\n");
         System.out.println("                 Terima Kasih atas kunjungan anda");
         System.out.println("                  ...Tekan Enter untuk Keluar...");
